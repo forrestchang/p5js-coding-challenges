f7b6321998eac8c8fab3e233408d0f57369ed527
Jiayuan Zhang (@Tisoga) <forrestchang7@gmail.com>
2017-05-09 20:59:54 +0800

    Update some code in StarField

--- Day001-StarField/Star.js
+++ Day001-StarField/Star.js
@@ -22,13 +22,13 @@ function Star() {
     var sy = map(this.y / this.z, 0, 1, 0, height);
     var r = map(this.z, 0, width, 20, 0);
 
-    ellipse(sx, sy, r, r);
+    // ellipse(sx, sy, r, r);
 
     var px = map(this.x / this.pz, 0, 1, 0, width);
     var py = map(this.y / this.pz, 0, 1, 0, height);
     this.pz = this.z;
 
-    // stroke(255);
-    // line(px, py, sx, sy);
+    stroke(255);
+    line(px, py, sx, sy);
   };
 }
