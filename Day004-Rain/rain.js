937c8f689f13220593823c3612ca2a4d8253c601
Jiayuan Zhang (@Tisoga) <forrestchang7@gmail.com>
2017-05-11 16:12:20 +0800

    Update Rains code

--- Day004-Rain/rain.js
+++ Day004-Rain/rain.js
@@ -1,34 +1,31 @@
 function Rain() {
-  this.z = random(1, 2);
+  this.x = random(width);
+  this.y = random(-1000, -500);
 
-  this.width = 2 * this.z;
-  this.height = random(20, 50);
-  this.x = random(0, width);
-  this.y = -this.height;
-  
-  this.velocity = random(0, 10);
-  this.gravity = 0.3 * this.z;
+  this.z = random(0.1, 10);
 
-  this.r = random(0, 255);
-  this.g = random(0, 255);
-  this.b = random(0, 255);
+  this.width = map(this.z, 0.1, 10, 0.5, 3);
+  this.height = random(5, 30);
+
+  this.speed = 0;
+  this.gravity = map(this.z, 0.1, 10, 0.1, 0.3);
 
   this.show = function() {
-    fill(this.r, this.g, this.b);
+    noStroke();
+    fill(234, 67, 53);
     rect(this.x, this.y, this.width, this.height);
   }
 
+  this.fall = function() {
+    this.speed += this.gravity;
+    this.y += this.speed;
+  }
+
   this.update = function() {
-    this.velocity += this.gravity;
-    this.y += this.velocity;
-
+    this.fall();
     if (this.y > height) {
-      this.y = -this.height;
-      this.velocity = 0;
-
-      this.r = random(0, 255);
-      this.g = random(0, 255);
-      this.b = random(0, 255);
+      this.y = random(-1000, -500);
+      this.speed = 0;
     }
   }
 }
--- Day004-Rain/sketch.js
+++ Day004-Rain/sketch.js
@@ -3,13 +3,13 @@ var rains = [];
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
 
-  for (var i = 0; i < 500; i++) {
+  for (var i = 0; i < 1000; i++) {
     rains.push(new Rain());
   }
 }
 
 function draw() {
-  background(0);
+  background(253, 253, 254);
 
   for (var i = 0; i < rains.length; i++) {
     rains[i].update();
