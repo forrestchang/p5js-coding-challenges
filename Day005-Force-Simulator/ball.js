451952efe624affb1c05539b6fd5eb1a0ce5da3b
Jiayuan Zhang (@Tisoga) <forrestchang7@gmail.com>
2017-05-16 10:38:45 +0800

    Update Force Simulator code

--- Day005-Force-Simulator/ball.js
+++ Day005-Force-Simulator/ball.js
@@ -1,21 +1,48 @@
-function Ball(x) {
-  this.r = 50;
-  this.x = width / 2;
-  this.y = height / 2;
+function Ball(m, x, y) {
+  this.mass = m;
+  this.position = createVector(x, y);
+  this.velocity = createVector(0, 0);
+  this.acceleration = createVector(0, 0);
 
-  this.velo = 1;
-  this.acce = 0.9;
-
-  this.t = 0;
-  this.delta_x = 0;
-
-  this.show = function() {
-    fill(255);
-    ellipse(this.x, this.y, this.r, this.r);
+  this.applyForce = function(force) {
+    var f = p5.Vector.div(force, this.mass);
+    this.acceleration.add(f);
   };
 
   this.update = function() {
-
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
   };
 
+  this.show = function() {
+    noStroke();
+    fill(255);
+    ellipse(this.position.x, this.position.y, this.mass, this.mass);
+  };
+
+  this.checkEdge = function() {
+    if (this.position.y > height - this.mass) {
+      this.velocity.y *= -0.9;
+      this.position.y = height - this.mass;
+    } else if (this.position.y - this.mass < 0) {
+      this.velocity.y *= -0.9;
+    }
+
+    if (this.position.x + this.mass > width) {
+      this.velocity.x *= -0.9;
+      this.position.x = width - this.mass;
+    } else if (this.position.x - this.mass < 0) {
+      this.velocity.x *= -0.9;
+      this.position.x = this.mass;
+    }
+  };
+
+  this.hits = function(otherBall) {
+    var distance = dist(this.position.x, this.position.y, otherBall.position.x, otherBall.position.y);
+    if (distance < this.mass + otherBall.mass) {
+      this.velocity.mult(-0.9);
+      otherBall.velocity.mult(-0.9);
+    }
+  };
 }
--- Day005-Force-Simulator/sketch.js
+++ Day005-Force-Simulator/sketch.js
@@ -1,13 +1,24 @@
-var ball;
+var balls = [];
+var time;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  ball = new Ball();
+
+  for (var i = 0; i < 10; i++) {
+    balls.push(new Ball(random(20, 50), random(width), random(height)));
+  }
 }
 
 function draw() {
   background(0);
 
-  ball.update();
-  ball.show();
+
+  for (var i = 0; i < balls.length; i++) {
+    var gravity = createVector(0, 0.1*balls[i].mass);
+    balls[i].applyForce(gravity);
+    balls[i].update();
+    balls[i].show();
+    balls[i].checkEdge();
+  }
+
 }
