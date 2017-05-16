d0bd670cb00d37aa45d09e56cdaba94744629d8c
Jiayuan Zhang (@Tisoga) <forrestchang7@gmail.com>
2017-05-16 13:20:14 +0800

    Add flag

--- Day005-Force-Simulator/sketch.js
+++ Day005-Force-Simulator/sketch.js
@@ -1,14 +1,18 @@
 var balls = [];
 var time;
+var flag;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   balls.push(new Ball(random(10, 50), random(width), random(height)));
+  flag = new Flag();
 }
 
 function draw() {
   background(0);
 
+  flag.update(mouseX, mouseY);
+  flag.show();
 
   for (var i = 0; i < balls.length; i++) {
     var gravity = createVector(map(mouseX, 0, width, -5, 5), map(mouseY, 0, height, -1, 1)*balls[i].mass);
@@ -23,4 +27,22 @@ function draw() {
 
 function mousePressed() {
   balls.push(new Ball(random(10, 50), mouseX, mouseY));
+}
+
+function Flag() {
+  this.x = width / 2;
+  this.y = height / 2;
+
+  this.x2 = width / 2;
+  this.y2 = width / 2;
+
+  this.show = function() {
+    fill(255);
+    line(this.x, this.y, this.x2, this.y2);
+  };
+
+  this.update = function(x, y) {
+    this.x2 = x;
+    this.y2 = y;
+  };
 }
